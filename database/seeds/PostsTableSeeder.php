9454bfef375754494f91d036dde901caaf1b596e
vagrant <gurico0516@yahoo.co.jp>
2020-10-13 14:43:44 +0000

    update files

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -13,12 +13,12 @@ class PostsTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(Post::class, 50)
-            ->create()
-            ->each(function ($post) {
-                $comments = factory(Comment::class, 2)->make();
-                $post->comments()->saveMany($comments);
-            }
-        );
+        // factory(Post::class, 50)
+        //     ->create()
+        //     ->each(function ($post) {
+        //         $comments = factory(Comment::class, 2)->make();
+        //         $post->comments()->saveMany($comments);
+        //     }
+        // );
     }
 }
