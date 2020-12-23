2cee98c511b98333bdcd6e77fff342faef01b38f
vagrant <gurico0516@yahoo.co.jp>
2020-12-23 14:38:07 +0000

    add function to base-controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -14,6 +14,30 @@ class Controller extends BaseController
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
     /**
+     * メニューからの遷移判別
+     *
+     * @var string
+     */
+    const FROM_MENU = 'menu';
+
+    /**
+     * メニューからの遷移か判別
+     * メニューのURLに？from=menuを追加してそのキー有無で判別
+     *
+     * @param string $fromMenuKey
+     * @return boolean true:メニューからの遷移 false:メニュー以外からの遷移
+     */
+    protected function isTransitionFromMenu(string $fromMenuKey)
+    {
+        $from = request()->query('from');
+        if (!is_null($from) && $from == self::FROM_MENU) {
+            Session::put($fromMenuKey, true);
+        }
+        return Session::get($fromMenuKey) === true;
+    }
+
+
+    /**
      * forward（画面初期表示 GET）でもredirect（入力 POST）同様にSession::flashInputを使用するとold inputのセッションが破棄されない
      * Session::nowを使用することで破棄させる
      *
